28e3322965a4041c073205b3142770059b741da7
akolisa <52515376+akolisa@users.noreply.github.com>
2019-07-05 17:40:20 +0800

    ip2019s

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,41 @@
 
 
 function start(e) {
+    var name=[
+        {
+            char:'簡',
+            big5:'C2B2',
+            cns:'1-767C',
+            unicode:'7C21'
 
+        },
+        {
+            char:'上',
+            big5:'A457',
+            cns:'1-4438',
+            unicode:'4E0A'
+        },
+        {
+            char:'博',
+            big5:'B3D5',
+            cns:'1-5E3B',
+            unicode:'535A'
+        }
+    ];
+
+    var ele=document.getElementById("div1");
+
+    for (var i=0;i<3;i++) {
+
+        ele.innerHTML+=name[i].char;
+        ele.innerHTML+=' big5= ';
+        ele.innerHTML+=name[i].big5;
+        ele.innerHTML+=' cns= ';
+        ele.innerHTML+=name[i].cns;
+        ele.innerHTML+=' unicode= ';
+        ele.innerHTML+=name[i].unicode;
+        ele.innerHTML+='<br>';
+    }
 }
 
 /*
